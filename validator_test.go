7761e9a5cfec142cf2cdff71d32a4f47f1f94223
dalei <guoyunlei@live.com>
2024-10-08 11:57:55 +0800

    Add test for custom conditional function

--- validator_test.go
+++ validator_test.go
@@ -35,3 +35,34 @@ func TestStruct(t *testing.T) {
 
 	fmt.Printf("Error in:\n%v\n", errors)
 }
+
+func TestConditional(t *testing.T) {
+	validateName := func(user User) bool {
+		structValidator := &StructValidator{}
+		allowAdminName := func() bool {
+			return user.Name != "admin"
+		}
+		structValidator.AddField(
+			"Name",
+			user.Name,
+			Conditional(allowAdminName, MinLength(6)),
+		)
+		return structValidator.Validate().HasErrors()
+	}
+
+	cases := []struct {
+		user     User
+		hasError bool
+	}{
+		{User{Name: ""}, true},
+		{User{Name: "dalei"}, true},
+		{User{Name: "admin"}, false},
+		{User{Name: "long name"}, false},
+	}
+	for _, c := range cases {
+		hasError := validateName(c.user)
+		if hasError != c.hasError {
+			t.Errorf("name `%s` conditional test result %t", c.user.Name, c.hasError)
+		}
+	}
+}
